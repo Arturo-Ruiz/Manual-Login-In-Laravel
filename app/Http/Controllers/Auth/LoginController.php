3d404ab9446faacb31c4c047f54d48f2d9569a3a
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2022-08-25 18:29:56 -0400

    login request ready now

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,30 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
+
+use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
-
-        $credentials = $request->validate([
-            'email' => ['required', 'email', 'string'],
-            'password' => ['required', 'string']
-        ]);
+    public function login(LoginRequest $request, Redirector $redirect){
 
         $remember = $request->filled('remember');
 
-        if (Auth::attempt($credentials, $remember)) {
+        if (Auth::attempt($request->only('email', 'password'), $remember)) {
+
             $request->session()->regenerate();
 
-            return redirect()
+            return $redirect
                 ->intended('dashboard')
                 ->with('status', 'You are logged in');
-                
         } else {
-            return redirect('login')->with('error', __('auth.failed'));
+            return $redirect
+                ->route('login')
+                ->with('error', __('auth.failed'));
         }
-
     }
 }
