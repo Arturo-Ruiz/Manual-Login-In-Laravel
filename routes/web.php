95f6d19216231d38dba897eb14d767f66c08efed
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2022-08-25 17:24:50 -0400

    add remember me session

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,17 @@ Route::view('/login', 'login')->name('login')->middleware('guest');;
 
 Route::view('/dashboard', 'dashboard')->middleware('auth');
 
-Route::post('login', function(){
+Route::post('login', function () {
+
     $credentials = request()->only('email', 'password');
 
-    if(Auth::attempt($credentials)){
+    $remember = request()->filled('remember');
+
+    if (Auth::attempt($credentials, $remember)) {
         request()->session()->regenerate();
 
         return redirect('dashboard');
-    }else{
+    } else {
         return redirect('login');
     }
-}); 
-
+});
